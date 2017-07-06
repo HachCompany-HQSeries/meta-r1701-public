0bcc35c5c76da3d25613125fc5bf354e9b375c70
Tim Higgins <thiggins@hach.com>
2017-07-06 14:11:55 -0600

    Introduced init and exit routines to control udev operations manually.

--- recipes-kernel/kernel-module-modbus/kernel-module-modbus/serial-modbus.c
+++ recipes-kernel/kernel-module-modbus/kernel-module-modbus/serial-modbus.c
@@ -28,20 +28,22 @@
 static const char DEVICE_NAME[] = "modbus_dev";
 static const char CLASS_NAME[] = "modbus_class";
 
-static struct cdev* my_dev;
+static struct cdev c_dev;
 static dev_t firstAssignedDevNum;                   // First device number being requested, major and minor.
-static const unsigned int firstOfMinorNum = 0;  
-static const unsigned int countOfMinorNum = 1;      // Range of contiguous minor numbers associated with this driver's 
+#define firstOfMinorNum 0  
+//static const unsigned int firstOfMinorNum = 0;  
+static const unsigned int countOfMinorNum = 2;      // Range of contiguous minor numbers associated with this driver's 
                                                     // major.
+static unsigned int nextAvailMinorNum = firstOfMinorNum;
 static struct class* modbusClass = NULL;            // class struct pointer.
 static struct device* modbusDevice = NULL;          // device struct pointer.
 
 static const int DEFAULT_BUFFER_SIZE = 1024;        // Size of communication buffer to support half duplex modbus. 
 
-// The following structure is used to contain data specific to opened instances of this device driver and maintainted
-// within the private_data field of the file structure.
+// Private device data.
 struct private_data {
-    int tempPlaceholder;
+    struct platform_device* pdev;
+    struct cdev my_dev;
     char* buffPtr;
     int buffMaxLen;
     int buffLen;
@@ -64,6 +66,7 @@ static int modbus_dev_open(struct inode* inode,     //!< [in] inode structure re
                                                     //!< structure in other entry points, you can access the same private
                                                     //!< data associated with that device node.
 {
+    /*
     struct private_data* myPrivateData;
 
     // Allocate memory for private data and associate into file structure specific to this open instance.
@@ -86,6 +89,7 @@ static int modbus_dev_open(struct inode* inode,     //!< [in] inode structure re
     file->private_data = myPrivateData;
 
 	pr_info("modbus_dev_open() is called.\n");
+    */
 	return 0;
 }
 
@@ -105,6 +109,7 @@ static int modbus_dev_close(struct inode* inode,    //!< [in] inode structure re
                                                     //!< structure in other entry points, you can access the same private
                                                     //!< data associated with that device node.
 {
+    /*
     struct private_data* myPrivateData;
     
     // Access private data associated with the file structure specific to this open instance.
@@ -120,6 +125,7 @@ static int modbus_dev_close(struct inode* inode,    //!< [in] inode structure re
     kfree(myPrivateData);
    
 	pr_info("modbus_dev_close() is called.\n");
+    */
 	return 0;
 }
 
@@ -133,6 +139,7 @@ static long modbus_dev_ioctl(struct file* file,
                              unsigned int cmd, 
                              unsigned long arg)
 {
+    /*
     struct private_data* myPrivateData;
     
     // Access private data associated with the file structure specific to this open instance.
@@ -146,6 +153,7 @@ static long modbus_dev_ioctl(struct file* file,
     // 1. Add feature to modify size of modbus buffer.
 
 	pr_info("modbus_dev_ioctl() is called. cmd = %d, arg = %ld\n", cmd, arg);
+    */
 	return 0;
 }
 
@@ -161,6 +169,7 @@ static ssize_t modbus_dev_read(struct file* file,   //!< [in] Open instance of a
                                size_t lbuf,         //!< [in] Number of bytes to read.
                                loff_t *ppos)        //!< [in, out] Position in buffer,
 {
+    /*
     struct private_data* myPrivateData;
     int nbytes;
     
@@ -195,6 +204,8 @@ static ssize_t modbus_dev_read(struct file* file,   //!< [in] Open instance of a
 
 	pr_info("modbus_dev_read() is called: nbytes=%d pos=%d.\n", nbytes, (int)*ppos);
     return nbytes;
+    */
+    return 0;
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
@@ -210,6 +221,7 @@ modbus_dev_write(struct file* file,                 //!< [in] Open instance of a
                  size_t lbuf,                       //!< [in] Size of the user space buffer.
                  loff_t *ppos)                      //!< [in, out] Position in buffer,
 {
+    /*
     struct private_data* myPrivateData;
     int nbytes;
     
@@ -232,6 +244,8 @@ modbus_dev_write(struct file* file,                 //!< [in] Open instance of a
 
 	pr_info("modbus_dev_write() is called: nbytes=%d pos=%d.\n", nbytes, (int)*ppos);
     return nbytes;
+    */
+    return 0;
 }
 
 // Define the entry points, or callback functions, implemented by this driver when a system call is made from user-space.
@@ -252,62 +266,89 @@ static const struct file_operations modbus_dev_fops = {
 //
 //! @retval int -
 //
-static int __init modbus_dev_probe(struct platform_device* pdev)
+static int modbus_dev_probe(struct platform_device* pdev)
 {
+    /*
     int ret;
+    dev_t nextAssignedDevNum;
+    static int nextAvailMinorNum = firstOfMinorNum;
+    struct private_data* pData;
+    */
 
-    pr_info("modbus_dev_probe() is called.\n");
+    pr_info("modbus_dev_probe() is called %d.\n", nextAvailMinorNum);
 
-	// Automatically allocate an available major and the range of minor numbers to go with it, return the first in
-    // firstAssignedDevNum.
-	ret = alloc_chrdev_region(&firstAssignedDevNum, firstOfMinorNum, countOfMinorNum, DEVICE_NAME); 
-    if (ret < 0){ 
-        pr_err("Failed to allocate device region.\n"); 
-        return ret; 
-    } 
-
-    // Allocate memory for the structure, initialize it, and sets the owner and ops fields to point to the current
-    // module and the proper file_operations table.
-    my_dev = cdev_alloc();
-    if(!my_dev) {
-        pr_err("Failed to allocate memory for device structure.\n");
+    modbusDevice = device_create(modbusClass, 
+                     NULL, 
+                     MKDEV(MAJOR(firstAssignedDevNum), nextAvailMinorNum), 
+                     NULL, 
+                     "%s%d", DEVICE_NAME, nextAvailMinorNum);
+    if(IS_ERR(modbusDevice)) {
+        //platform_driver_unregister(&my_platform_driver); 
+        pr_info("Failed to create the device.\n");
+        //class_destroy(modbusClass);
+        //unregister_chrdev_region(firstAssignedDevNum, countOfMinorNum);
         return -1;
     }
-	cdev_init(my_dev, &modbus_dev_fops);
+    nextAvailMinorNum++;
 
-    // Make the driver live in the system.
-	ret= cdev_add(my_dev, firstAssignedDevNum, countOfMinorNum);
-	if(ret < 0) {
-		pr_err("Unable to add cdev\n");
-        cdev_del(my_dev);
-		unregister_chrdev_region(firstAssignedDevNum, countOfMinorNum);
-		return ret;
-	}
+    /*
+	// Automatically allocate an available major and the range of minor numbers to go with it, return the first in
+    // firstAssignedDevNum.
+    if(nextAvailMinorNum == 0) {
+        ret = alloc_chrdev_region(&firstAssignedDevNum, firstOfMinorNum, countOfMinorNum, DEVICE_NAME); 
+        if (ret < 0){ 
+            pr_err("Failed to allocate device region.\n"); 
+            return ret; 
+        }
+    } 
+
+    // Allocate memory for the private data, initialize it, and sets the owner and ops fields to point to the current
+    // module and the proper file_operations table.
+    pData = devm_kzalloc(&pdev->dev, sizeof(struct private_data), GFP_KERNEL);
+    if(!pData) {
+        pr_err("Failed to allocate memory for device structure.\n");
+        return -ENOMEM;
+    }
+	pData->pdev = pdev;
+    cdev_init(&pData->my_dev, &modbus_dev_fops);
+
+    // To access private data in other parts of the driver, attach it to the pdev structure.
+    platform_set_drvdata(pdev, pData);
+    
+    // Make the kernel aware of the driver.
+    ret = cdev_add(&pData->my_dev, firstAssignedDevNum, countOfMinorNum);
+    if(ret < 0) {
+        pr_err("Unable to add cdev\n");
+        unregister_chrdev_region(firstAssignedDevNum, countOfMinorNum);
+        return ret;
+    }
 
     // Register the device class.
-	modbusClass = class_create(THIS_MODULE, CLASS_NAME);
-	if(IS_ERR(modbusClass)) {
-	    pr_err("Failed to register device class.\n");
-        cdev_del(my_dev);
-		unregister_chrdev_region(firstAssignedDevNum, countOfMinorNum);
-	    return PTR_ERR(modbusClass);
-	}
+    if(nextAvailMinorNum == 0) {
+        modbusClass = class_create(THIS_MODULE, CLASS_NAME);
+        if(IS_ERR(modbusClass)) {
+            pr_err("Failed to register device class.\n");
+            unregister_chrdev_region(firstAssignedDevNum, countOfMinorNum);
+            return PTR_ERR(modbusClass);
+        }
+    }
 
     // Create the device and register a node in sysfs.
-	modbusDevice = device_create(modbusClass, NULL, firstAssignedDevNum, NULL, "%s%d", DEVICE_NAME,
-                                                                                       MINOR(firstAssignedDevNum));
+    nextAssignedDevNum = MKDEV(MAJOR(firstAssignedDevNum), nextAvailMinorNum);
+	modbusDevice = device_create(modbusClass, NULL, nextAssignedDevNum, NULL, "%s%d", DEVICE_NAME,
+                                                                                      nextAvailMinorNum++);
 	if(IS_ERR(modbusDevice)) {
 	    pr_info("Failed to create the device.\n");
 	    class_destroy(modbusClass);
-        cdev_del(my_dev);
 		unregister_chrdev_region(firstAssignedDevNum, countOfMinorNum);
 	    return PTR_ERR(modbusDevice);
 	}
    
     dev_info(modbusDevice, "Succeeded in registering character device %s, Major=%d, Minor=%d.\n", DEVICE_NAME,
                                                                                                   MAJOR(firstAssignedDevNum),
-                                                                                                  MINOR(firstAssignedDevNum));
+                                                                                                  nextAvailMinorNum);
 
+    */
     return 0;
 }
 
@@ -320,10 +361,8 @@ static int __exit modbus_dev_remove(struct platform_device* pdev)
 {
 	pr_info("modbus_dev_remove() is called.\n");
 
+    /*
     // Remove the device from the system.
-    if(my_dev) {
-        cdev_del(my_dev);
-    }
     device_destroy(modbusClass, firstAssignedDevNum);
 
     // Unregister the device class.
@@ -335,6 +374,7 @@ static int __exit modbus_dev_remove(struct platform_device* pdev)
     // Unregister the device numbers and remove the association with device numbers.
     unregister_chrdev_region(firstAssignedDevNum, countOfMinorNum);
 
+    */
     return 0;
 }
 
@@ -358,7 +398,108 @@ static struct platform_driver my_platform_driver = {
 };
 
 // Register our platform 'device' driver with the platform 'bus' driver.
-module_platform_driver(my_platform_driver);
+//module_platform_driver(my_platform_driver);
+
+
+static int __init modbus_dev_init(void)
+{
+    int ret;
+    //int minorNum;
+
+    pr_info("modbus_dev_init() is called.\n");
+    nextAvailMinorNum = firstOfMinorNum;
+    //ret = platform_driver_probe(&my_platform_driver, &modbus_dev_probe);
+    //ret = platform_driver_register(&my_platform_driver);
+    //if (ret < 0){ 
+    //    pr_err("Call to platform_driver_probe failed with: %d.\n", ret); 
+    //    return ret; 
+    //} 
+
+    // Automatically allocate an available major and the range of minor numbers to go with it, return the first in
+    // firstAssignedDevNum.
+    ret = alloc_chrdev_region(&firstAssignedDevNum, firstOfMinorNum, countOfMinorNum, DEVICE_NAME); 
+    if (ret < 0){
+        platform_driver_unregister(&my_platform_driver); 
+        pr_err("Failed to allocate device region.\n"); 
+        return ret; 
+    } 
+
+    // Register the device class.
+    modbusClass = class_create(THIS_MODULE, CLASS_NAME);
+    if(IS_ERR(modbusClass)) {
+        platform_driver_unregister(&my_platform_driver); 
+        pr_err("Failed to register device class.\n");
+        unregister_chrdev_region(firstAssignedDevNum, countOfMinorNum);
+        return PTR_ERR(modbusClass);
+    }
+
+    // Create the device and register a node in sysfs.
+    /*
+    for(minorNum=MINOR(firstAssignedDevNum); minorNum<countOfMinorNum; minorNum++) {
+        if(device_create(modbusClass, 
+                         NULL, 
+                         MKDEV(MAJOR(firstAssignedDevNum), MAJOR(firstAssignedDevNum) + minorNum), 
+                         NULL, 
+                         "%s%d", DEVICE_NAME, minorNum) == NULL) {
+            platform_driver_unregister(&my_platform_driver); 
+            pr_info("Failed to create the device.\n");
+            class_destroy(modbusClass);
+            unregister_chrdev_region(firstAssignedDevNum, countOfMinorNum);
+            return PTR_ERR(modbusDevice);
+        }
+	}
+    */
+
+    cdev_init(&c_dev, &modbus_dev_fops);
+    
+    // Make the kernel aware of the driver.
+    ret = cdev_add(&c_dev, firstAssignedDevNum, countOfMinorNum);
+    if(ret < 0) {
+        platform_driver_unregister(&my_platform_driver); 
+        pr_err("Unable to add cdev.\n");
+        //for(minorNum=MINOR(firstAssignedDevNum); minorNum<countOfMinorNum; minorNum++) {
+        //    device_destroy(modbusClass, MKDEV(MAJOR(firstAssignedDevNum), MAJOR(firstAssignedDevNum) + minorNum));
+        //}
+	    class_unregister(modbusClass);
+        unregister_chrdev_region(firstAssignedDevNum, countOfMinorNum);
+        return ret;
+    }
+
+    ret = platform_driver_register(&my_platform_driver);
+    if (ret < 0){ 
+        pr_err("Call to platform_driver_probe failed with: %d.\n", ret); 
+        return ret; 
+    } 
+    return 0;
+}
+
+static void __exit modbus_dev_exit(void)
+{
+    int minorNum;
+
+	pr_info("modbus_dev_exit() is called.\n");
+
+    platform_driver_unregister(&my_platform_driver); 
+
+    cdev_del(&c_dev);
+
+    // Remove the device from the system.
+    for(minorNum=MINOR(firstAssignedDevNum); minorNum<countOfMinorNum; minorNum++) {
+        device_destroy(modbusClass, MKDEV(MAJOR(firstAssignedDevNum), MAJOR(firstAssignedDevNum) + minorNum));
+    }
+
+    // Unregister the device class.
+	class_unregister(modbusClass);
+
+    // Remove the device class.
+	class_destroy(modbusClass);
+    
+    // Unregister the device numbers and remove the association with device numbers.
+    unregister_chrdev_region(firstAssignedDevNum, countOfMinorNum);
+}
+
+module_init(modbus_dev_init);
+module_exit(modbus_dev_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Tim Higgins");
