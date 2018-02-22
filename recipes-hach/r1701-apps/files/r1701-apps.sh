54f6303c6bfd3a4d41cf7b0728d8eb2576b601d8
Devang Patel <dpatel@hach.com>
2018-02-22 07:55:05 -0700

    Fix for  GUI manager to start properly via systemV init script. Linux environment arguments are not set for Qt at boot time. Once user log in then only gui was working. This fix is important for running apps via startup scripts.

--- recipes-hach/r1701-apps/files/r1701-apps.sh
+++ recipes-hach/r1701-apps/files/r1701-apps.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 case "$1" in
   start)
-        start-stop-daemon -S -q -m -p /var/run/sys_mgr.pid --exec /usr/bin/sys_mgr -- -d
-        echo "OK"
+        export QT_QPA_PLATFORM=linuxfb:fb=/dev/fb0
+        export QT_QPA_FB_DISABLE_INPUT=1
+        export QMLSCENE_DEVICE=softwarecontext
+        start-stop-daemon --start --quiet --make-pidfile --pidfile /var/run/sys_mgr.pid --exec /usr/bin/sys_mgr -- -d
+        echo "r1701 - started as deamon"
         ;;
   stop)
-        start-stop-daemon -K -q -p /var/run/sys_mgr.pid
-        echo "OK"
+        start-stop-daemon --stop --quiet --pidfile /var/run/sys_mgr.pid
+        echo "r1701 - deamon stopped"
         ;;
   *)
         echo "Usage: $0 {start|stop}"
