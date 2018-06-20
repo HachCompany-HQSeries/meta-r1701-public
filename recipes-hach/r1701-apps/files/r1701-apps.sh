f7bf1a42161cb1faafdfe6416315e66677e7de35
ryang <ryang@hach.com>
2018-06-20 13:11:20 -0600

    Added the display selection based on the uboot model type variable

--- recipes-hach/r1701-apps/files/r1701-apps.sh
+++ recipes-hach/r1701-apps/files/r1701-apps.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 case "$1" in
   start)
-        export QT_QPA_PLATFORM=linuxfb:fb=/dev/fb0
+		uboot-model-type="model-type"
+		model-type="HPP"
+ 		eval "${model-type}=\"$(fw_printenv -n ${uboot-model-type} 2>/dev/null)\"" 
+		if [ "${model-type}" = "MPP" ]; then
+			modprobe -r mxsfb
+			export QT_QPA_PLATFORM=linuxfb:fb=/dev/fb1
+		else
+			modprobe -r mipdisplay-3-2-inch
+			export QT_QPA_PLATFORM=linuxfb:fb=/dev/fb0
+		fi      
+
         export QT_QPA_FB_DISABLE_INPUT=1
         export QMLSCENE_DEVICE=softwarecontext
         start-stop-daemon --start --quiet --make-pidfile --pidfile /var/run/sys_mgr.pid --exec /opt/hach/bin/sys_mgr -- -d
